a05b02cec1cbb66b9e3d8d9911efd099dd276365
Aleksandr Zelenkov <saneckzs@yandex.ru>
2023-06-25 12:27:04 +0300

    ПР14 Итерация 1.3.1

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,8 @@ const routerCards = require('./cards');
 
 router.post('/signup', validateSignUp, createUser);
 router.post('/signin', validateSignIn, login);
-
-router.use(auth);
-
-router.use(routerUsers);
-router.use(routerCards);
+router.use('/users', auth, routerUsers);
+router.use('/cards', auth, routerCards);
 router.use('/*', () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
