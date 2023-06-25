dd9cb8ac39ea08a34e223d60b584bbfb0fc9a2c1
Aleksandr Zelenkov <saneckzs@yandex.ru>
2023-06-25 13:21:28 +0300

    ПР 14 Итерация 1.4

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const handleError = require('./middlewares/handleError');
 
 const { PORT = 3000 } = process.env;
+const { MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
+mongoose.connect(MONGO_URL)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
