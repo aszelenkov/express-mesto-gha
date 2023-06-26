aa568a2062559eddb98d7a60d081cdbd7c0c9814
Aleksandr Zelenkov <saneckzs@yandex.ru>
2023-06-26 12:57:20 +0300

    GH14 Итерация 2.4

--- models/user.js
+++ models/user.js
@@ -3,42 +3,45 @@ const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Жак-Ив Кусто',
-  },
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Исследователь',
-  },
-  avatar: {
-    type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректный URL аватара',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      default: 'Жак-Ив Кусто',
+    },
+    about: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      default: 'Исследователь',
+    },
+    avatar: {
+      type: String,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: 'Некорректный URL аватара',
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'Неверный формат почты',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (v) => validator.isEmail(v),
-      message: 'Неверный формат почты',
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-});
+  { versionKey: false },
+);
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
   return this.findOne({ email }).select('+password')
